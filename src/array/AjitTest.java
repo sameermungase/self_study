75bc6d9eb29a426b1d995e03185d8fd37325ebe3
AJIT <ajitthakare123@gmail.com>
2023-10-01 14:41:04 +0530

    test branch push and Pull request

---
+++ src/array/AjitTest.java
@@ -0,0 +1,7 @@
+package array;
+
+public class AjitTest {
+    public static void main(String[] args) {
+        System.out.println("Ajit");
+    }
+}
--- src/array/compareArrays.java
+++ src/array/compareArrays.java
@@ -12,6 +12,7 @@ public class compareArrays {
         for (int i = 0; i < array1.length; i++) {
             for (int j = 0; j < array2.length; j++) {
                 if (array1[i] == array2[j]) list.add(array2[j]);
+
             }
         }
         
